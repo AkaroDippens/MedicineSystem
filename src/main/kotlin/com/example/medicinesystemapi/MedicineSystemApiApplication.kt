94993b7df2fd0d59ccacd64e2dda83cc7d27d5a5
AkaroDippens <mirzoev.nidzhat@mail.ru>
2024-10-25 18:46:22 +0300

    Initial commit

---
+++ src/main/kotlin/com/example/medicinesystemapi/MedicineSystemApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.medicinesystemapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MedicineSystemApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<MedicineSystemApiApplication>(*args)
+}
